b82dd6b00206a7c99850913ca8eb2d332ad388b1
florylsk <florilsk@yahoo.es>
2022-03-03 10:21:42 +0100

    yes

--- hw/dv/dpi/gpiodpi/headers/tomcrypt_custom.h
+++ hw/dv/dpi/gpiodpi/headers/tomcrypt_custom.h
@@ -161,10 +161,10 @@
 #ifndef LTC_NO_MATH
 
 /* LibTomMath */
-#define LTM_DESC
+//#define LTM_DESC
 
 /* TomsFastMath */
-/* #define TFM_DESC */
+#define TFM_DESC
 
 /* GNU Multiple Precision Arithmetic Library */
 /* #define GMP_DESC */
